085e22350dfa9821b74f36a405d63e6b269414e2
Millard Frédéric <fmi@octo.com>
2015-06-19 17:47:14 +0200

    config pour que tout marche avec un mvn test

--- src/test/java/api/cucumber/CucumberRunnerTest.java
+++ src/test/java/api/cucumber/CucumberRunnerTest.java
@@ -1,9 +1,11 @@
 package api.cucumber;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"}, tags = {"~@ignore"}, features = "src/test/resources/")
 public class CucumberRunnerTest {
 
 }
