861126204c0ced14d6de821e9c26dc67edb678c2
Millard Frédéric <fmi@octo.com>
2015-07-10 11:22:42 +0200

    renommage des test

--- src/test/java/api/disponibilite/ApiUnitaireDisponibiliteTest.java
+++ src/test/java/api/disponibilite/ApiUnitaireDisponibiliteTest.java
@@ -13,26 +13,26 @@ import static org.hamcrest.CoreMatchers.is;
 public class ApiUnitaireDisponibiliteTest extends BaseTest {
 
     @Test
-    public void testGetAllCoupons() {
+    public void GET_coupons() {
         ResponseEntity<Coupon[]> response = getAllCoupons();
         assertAndLogTempsDeReponse(response.getStatusCode(), HttpStatus.OK, "/v1/coupons/", "GET");
     }
 
     @Test
-    public void testPostACoupons() {
+    public void POST_coupons() {
         ResponseEntity<Coupon> response = postCouponResponseEntity("nom testPostACoupons", "reduction testPostACoupons", true);
         assertAndLogTempsDeReponse(response.getStatusCode(), HttpStatus.CREATED, "/v1/coupons/", "POST");
     }
 
     @Test
-    public void testGetACoupons() {
+    public void GET_coupons_byId() {
         ResponseEntity<Coupon> postResponse = postCouponResponseEntity("nom testPostACoupons", "reduction testPostACoupons", true);
         ResponseEntity<Coupon> response = getCouponResponseEntity(postResponse.getBody().getId());
         assertAndLogTempsDeReponse(response.getStatusCode(), HttpStatus.OK, "/v1/coupons/{couponId}", "GET");
     }
 
     @Test
-    public void testDeleteACoupons() {
+    public void DELETE_coupons() {
         ResponseEntity<Coupon> postResponse = postCouponResponseEntity("nom testPostACoupons", "reduction testPostACoupons", true);
         ResponseEntity<String> response = deleteCouponResponseEntity(postResponse.getBody().getId());
         assertAndLogTempsDeReponse(response.getStatusCode(), HttpStatus.NO_CONTENT, "/v1/coupons/{couponId}", "DELETE");
