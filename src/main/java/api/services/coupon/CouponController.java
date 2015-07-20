797458787befa91e4f2918e569b83790a614c0e4
Millard Frédéric <fmi@octo.com>
2015-07-20 10:36:13 +0200

    Rollback : Commit un truc qui compile pas pour tester l'UDD

--- src/main/java/api/services/coupon/CouponController.java
+++ src/main/java/api/services/coupon/CouponController.java
@@ -101,7 +101,7 @@ public class CouponController extends BaseMockController {
             LOGGER.info("OUT - deleteCoupon => Erreur 404");
             throw new CouponNotFoundException(couponId);
         }
-        dataManager.supprimerCouposxn(couponId);
+        dataManager.supprimerCoupon(couponId);
         LOGGER.info("OUT - deleteCoupon");
         return true;
     }
